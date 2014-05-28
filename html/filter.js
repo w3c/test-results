ce572de60956c3c53d5ced87b3df69a5e6ad71ad
Robin Berjon <robin@berjon.com>
2014-05-28 18:10:05 +0200

    opera results

--- html/filter.js
+++ html/filter.js
@@ -8,6 +8,7 @@ exports.excludeFile = function (file) {
 };
 
 exports.excludeCase = function (file, name) {
+    if (name === "null") return true;
     if (/Table/.test(name) && /(sort|stopSorting)/.test(name)) return true;
     if (/sortable|sorted/.test(name)) return true;
     if (/HTML(Menu|Details|Dialog)/.test(name)) return true;
@@ -15,10 +16,11 @@ exports.excludeCase = function (file, name) {
     if (/is(Protocol|Content)HandlerRegistered/.test(name)) return true;
     if (/unregister(Protocol|Content)Handler/.test(name)) return true;
     if (/cssElementMap|contextMenu|forceSpellCheck/.test(name)) return true;
-    if (/scoped|command|srcset|seamless|:dir|:progress/.test(name)) return true;
+    if (/scoped|command|srcset|seamless|:dir|:progress|dialog/.test(name)) return true;
     if (/(before|append|after|replace)\(/.test(name)) return true;
     if (/HD\(/.test(name)) return true;
-    if (/(transferControlToProxy|setContext|supportsContext)/.test(name)) return true;
+    if (/transferControlToProxy|setContext|supportsContext/.test(name)) return true;
+    if (/item(Id|Scope|Type|Ref|Prop|Value)/.test(name)) return true;
     if (/(CanvasRenderingContext2D|CanvasPattern|TextMetrics|DrawingStyle|CanvasProxy|Path|ImageBitmap)/.test(name)) return true;
     if (/(valueAsDate|inputMode)/.test(name)) return true;
     if (/Worker|WebSocket|MessageChannel|MessagePort|PortCollection|CloseEvent|MessageEvent|Storage|EventSource/.test(name)) return true;
@@ -29,7 +31,5 @@ exports.excludeCase = function (file, name) {
     // CONTENTIOUS
     // I'm pretty sure the tabIndex tests aren't right
     if (/tabIndex/i.test(name)) return true;
-    // the track.itemValue tests look fishy
-    if (/itemValue/i.test(name)) return true;
     return false;
 };
