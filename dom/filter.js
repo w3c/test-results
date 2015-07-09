b323e5d1b7a1f21be0ba63935de14bd75fe7586a
Robin Berjon <robin@berjon.com>
2015-07-09 16:15:40 +0200

    updated the details analysis

--- dom/filter.js
+++ dom/filter.js
@@ -17,5 +17,9 @@ exports.excludeCase = function (file, name) {
     if (/\bafter\b/.test(name)) return true;
     if (/\bbefore\b/.test(name)) return true;
     if (/\bquery(?:All)\b/.test(name)) return true;
+    // these basically repeat the tests
+    if (/\binputEncoding\b/.test(name)) return true;
+    // those are testing Encoding
+    if (/macintosh|windows-|x-mac|gbk|hz|big5|csiso2022kr|iso-2022-cn|iso-2022-kr/i.test(name)) return true;
     return false;
 };
